520ddde12ca033bd511d9f769478d71e93a54359
skinnybuff <s_t_white@yahoo.com>
2018-01-28 22:28:04 -0500

    Add sign in and sign up btn events

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -1,7 +1,7 @@
 'use strict'
-
 const setAPIOrigin = require('../../lib/set-api-origin')
 const config = require('./config')
+const event = require('./events')
 
 $(() => {
   setAPIOrigin(location, config)
@@ -12,3 +12,10 @@ $(() => {
 
 // use require without a reference to ensure a file is bundled
 // require('./example')
+
+$(() => {
+  // connect the sign up btn to the sign function
+  $('#sign-up').on('submit', event.onSignUp)
+  //connenct the sign in btn to the sign in function
+  $('#sign-in').on('submit', event.onSignIn)
+})
