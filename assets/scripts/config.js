cdedc5eae4e1701a01652b75fcd6f64fa5b3a218
skinnybuff <s_t_white@yahoo.com>
2018-02-08 13:00:05 -0500

    Add correct herko deployment url

--- assets/scripts/config.js
+++ assets/scripts/config.js
@@ -2,7 +2,7 @@
 
 const config = {
   apiOrigins: {
-    production: 'https://git.heroku.com/local-league.git',
+    production: 'https://local-league.herokuapp.com/',
     development: 'http://localhost:4741'
   }
 }
