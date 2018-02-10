e1d0df244648b0b50dc140b09b3997ec739704f0
skinnybuff <s_t_white@yahoo.com>
2018-02-10 17:43:59 -0500

    Add ui re-submittal changes
    added HUD display for signup success
    close update game function to handle event and ui to close update form
    event to handle the showing and hiding of the password change form
    show game edit function added to hide and show create game while editing a game

--- assets/scripts/ui.js
+++ assets/scripts/ui.js
@@ -15,51 +15,75 @@ const apiFailure = function (error) {
 const signInSuccess = function (data) {
   // the sign in return data is saved in the app store
   store.user = data.user
-  // show the list of  games displayed on sign in\
+  // show the list of  games displayed on sign in
   $('#gamesDisplay').fadeIn('slow')
+  // show option to sign out and hide sign in/up forms
   $('#signOutForm').show()
   $('#signUpForm').fadeOut('fast')
   $('#signInForm').hide()
   $('#signUpToggleBtn').hide()
+  // show form to create new games
   $('#gameCreateForm').show()
+  // show user email in the signout field
   $('#signOutForm input').val(data.user.email)
-  $('#changePassword').show()
+  // hide change password option until toggled
+  $('#changePassword').hide()
+  // clear the  HUD if any feed back was logged
   $('#uiFeedbackDisplay').html('')
+  // clear the sign in form values
   $('#signInForm :input').val('')
 }
 
 const signInFailure = function (data) {
+  // clear the HUD
   $('#uiFeedbackDisplay').text('')
-  $('#uiFeedbackDisplay').text('Please try again')
+  // User feed back for a sign in error
+  $('#uiFeedbackDisplay').text('Please sign up or check your email and password')
 }
 
 const signOutSuccess = function (data) {
+  // on sign out clear in app data store
+  store.user = {}
+  store.games = {}
+  // clear the games resource dispaly
   $('#gamesDisplay').fadeOut('slow')
-  $('#signOutForm').hide()
+  // clear the HUD
+  $('#uiFeedbackDisplay').text('')
+  // Show the option to log back in
   $('#signInForm').show()
+  // hide all other forms
+  $('#signOutForm').hide()
   $('#gameCreateForm').hide()
   $('#changePassword').hide()
-  $('#uiFeedbackDisplay').text('')
+  $('#gameUpdateForm').hide()
 }
 
 const signUpSuccess = function () {
   $('#uiFeedbackDisplay').text('')
-  $('#uiFeedbackDisplay').text('You have Signed Up')
+  $('#uiFeedbackDisplay').text('You succsefuly Signed Up please Sign In')
+  // hide sign up form to prevent error from signing up twice
   $('#signUpForm').hide()
 }
 
 const signUpFailure = function () {
+  // clear hud
   $('#uiFeedbackDisplay').text('')
-  $('#uiFeedbackDisplay').text('Please try and sign up again')
+  // feed back for bad sign up
+  $('#uiFeedbackDisplay').text('Please try a differrent email and password')
 }
 
 const changePassSuccess = function () {
+  // clear HUD
   $('#uiFeedbackDisplay').text('')
-  $('#uiFeedbackDisplay').text('Password Changed').fadeIn('fast')
+  // HUD feed back on password patch success
+  $('#uiFeedbackDisplay').text('Password Changed')
+  // pasword input values are cleared
+  $('#changePassword :input').val('')
+  // the change password UI feedback will hide after 1.6 seconds
   window.setTimeout(() => {
     $('#uiFeedbackDisplay').text('')
+    $('#changePassword').hide()
   }, 1600)
-  $('#changePassword :input').val('')
 }
 
 const getAllGamesSuccess = function (data) {
@@ -80,12 +104,30 @@ const deleteGameSuccess = function () {
 }
 
 const gamePatchSuccess = function () {
+  // empty the games display
+  // its repopulated on the event chain with a call for all games
   $('#gamesDisplay').empty()
   $('#gameUpdateForm').hide()
   $('#gameCreateForm').show()
+  // clear update game form
   $('#gameUpdateForm :input').val('')
 }
 
+const closeUpdateGame = () => {
+  console.log('ui update event')
+  $('#gameUpdateForm').hide()
+  $('#gameCreateForm').show()
+}
+
+const toggleChangePassword = () => {
+  $('#changePassword').toggle()
+}
+
+const showGameEditForm = () => {
+  $('#gameCreateForm').hide()
+  $('#gameUpdateForm').show()
+  $('#updateCloseBtn').show()
+}
 module.exports = {
   apiFailure,
   signInSuccess,
@@ -97,5 +139,8 @@ module.exports = {
   gamePatchSuccess,
   deleteGameSuccess,
   createGameSuccess,
-  signUpSuccess
+  signUpSuccess,
+  closeUpdateGame,
+  toggleChangePassword,
+  showGameEditForm
 }
